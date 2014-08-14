aa43ae9b2470aafeecdbac8f8e16a9d9fc74cb56
Zia Ur Rehman <zia@brainloggers.co.uk>
2014-08-15 03:17:46 +0500

    kickstarted the project
    * added express.js
    * added less-middleware support for express.js
    * added assets/css to .gitignore
    * added jquery and bootstrap (sorry, bower just isn't feasible)
    * added a .jshintrc template
    * created `layout`, `error` and `index` view templates

---
+++ app.js
@@ -0,0 +1,87 @@
+/*
+ *
+ */
+
+// requires, requires
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    less = require('less-middleware'),
+
+    // route up!
+    routes = require('./routes/index'),
+
+    // the app object.
+    app = express();
+
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+// enter the dreaded less-middleware!
+app.use(less(path.join(__dirname, 'assets', 'less'), {
+  compiler: {
+    yuicompress: app.get('env') === 'development' ? false : true
+  },
+  debug: app.get('env') === 'development' ? true : false,
+  dest: path.join(__dirname, 'assets'),
+  force: app.get('env') === 'development' ? true : false,
+  once: app.get('env') === 'production' ?  true : false,
+  // see: http://git.io/x_5jrw
+  preprocess: {
+    path: function (pname, req) {
+      if (path.sep === '\\') {
+        return pname.replace('\\css\\', '\\');
+      } else {
+        return pname.replace('\/css\/', '\/');
+      }
+    }
+  }
+}));
+
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
