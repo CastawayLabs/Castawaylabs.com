abe33010f1311720bc09542517e703769eecb8d7
DarkSeraphim <datfireblast709@gmail.com>
2015-03-08 22:11:10 +0100

    Chore: fixing the style a bit

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express'),
-    nodemailer = require('nodemailer');
-var router = express.Router();
-var transporter = nodemailer.createTransport({
+    nodemailer = require('nodemailer'),
+    router = express.Router(),
+    transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
             user: process.env.EMAIL_USER || '',
             pass: process.env.EMAIL_PASS || ''
         }
-    });
-var emailValidator = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
-var EMAIL_TO = process.env.EMAIL_TO || '';
+    }),
+    sendMail = transporter.sendMail.bind(transporter),
+    emailValidator = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i,
+    EMAIL_TO = process.env.EMAIL_TO || '';
 if (!EMAIL_TO || !emailValidator.test(EMAIL_TO)) {
     throw new Error("Invalid EMAIL_TO.");
 }
-var sendMail = transporter.sendMail.bind(transporter);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -23,9 +23,9 @@ router.get('/', function(req, res) {
 });
 
 router.post('/email', function(req, res) {
-  var name = req.body.inputName || '';
-  var email = req.body.inputEmail || '';
-  var message = req.body.inputMessage || '';
+  var name = req.body.inputName || '',
+      email = req.body.inputEmail || '',
+      message = req.body.inputMessage || '';
 
   if(!name || !email || !message || !emailValidator.test(email)) { // Add more validation?
     res.send('nope');
