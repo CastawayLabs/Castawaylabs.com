998c9e9d2bcf657150db892a2e64dc9ba8099bc3
Anže Jenšterle <jensterle.anze@gmail.com>
2015-07-10 20:43:51 +0200

    Updated to fit new express rules

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(less(path.join(__dirname, 'assets', 'less'), {
